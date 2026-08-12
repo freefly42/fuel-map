2a34258e1ce82d823602ac675a4628cfa2f62d3b
Kent Hulick <Kent@Texas-Pilot.com>
2026-08-12 22:35:41 +0000

    Keep mobile map controls inline

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "fuel-route-shell-v63";
+const SHELL_CACHE = "fuel-route-shell-v64";
 const SHELL = ["./", "./index.html", "./app.css", "./app.js", "./RedPlane.svg", "./texas-pilot-logo.png", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
