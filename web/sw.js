4c9afdb1a7bb7dbf70f69bacce6b3ee951ed61fc
Kent Hulick <Kent@Texas-Pilot.com>
2026-08-12 04:13:51 +0000

    Restore flight settings title

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "fuel-route-shell-v43";
+const SHELL_CACHE = "fuel-route-shell-v44";
 const SHELL = ["./", "./index.html", "./app.css", "./app.js", "./texas-pilot-logo.png", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
